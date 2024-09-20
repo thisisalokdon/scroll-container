a6f3fad5b976c7703e1a5d44c959fbb2c005823a
Er. Alok Tripathi <thisisalokdon@gmail.com>
2024-09-20 06:16:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const scrollContainer = document.querySelector('.scroll-content');
+function checkWorkingHours() {
+    var now = new Date();
+    var hours = now.getUTCHours() + 5; // Add 5 hours for Indian time zone offset
+    var minutes = now.getUTCMinutes() + 30; // Add 30 minutes for Indian time zone offset
 
-// Clone the icons to create a continuous scroll effect
-const clone = scrollContainer.cloneNode(true);
-scrollContainer.parentNode.appendChild(clone);
+    if (minutes >= 60) {
+        hours += 1;
+        minutes -= 60;
+    }
+
+    var startHour = 11; // 11:00 AM
+    var endHour = 18; // 6:00 PM
+
+    if (hours > startHour || (hours == startHour && minutes >= 0)) {
+        if (hours < endHour || (hours == endHour && minutes <= 0)) {
+            document.getElementById("call-working").style.display = "none";
+            return;
+        }
+    }
+
+    document.getElementById("call-working").style.display = "flex";
+}
+
+checkWorkingHours();
+setInterval(checkWorkingHours, 60000);
